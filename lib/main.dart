70a7b5632f3e3531fdd4d98c755b63a9f8a7680f
JoanPerezPerez <joan.perez.p@estudiantat.upc.edu>
2025-03-17 11:23:36 +0100

    feat: scrollbar + fix: centrar txt segona pantalla

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'api/models/new_model.dart';
@@ -52,7 +51,6 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-
     return Scaffold(
       body: Center(
         child: Column(
@@ -133,74 +131,80 @@ class NewsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Noticia al detall'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Flexible( // premet que el text s'adapti a la pantalla
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    news.name ?? 'No title for this new',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      
+      body: SingleChildScrollView(  // si per algun motiu en la descripció + imatge hi ha molt text fa apareixer una scrollbar per poder baixar.
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center, // alinem al text de descripció al centre de la pantalla
+            children: [
+              Flexible( // premet que el text s'adapti a la pantalla
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      news.name ?? 'No title for this new',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        
+                      ),
+                      softWrap: true, // trenca la linia si es necesari
                     ),
-                    softWrap: true, // trenca la linia si es necesari
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    news.description ?? 'No description for this new', 
-                    style: TextStyle(fontSize: 16),
-                    softWrap: true, // trenca la linia si es necesari
-                  ),
-                  SizedBox(height: 8),
-                  Column(
-                    children: [
-                      if (news.image != null)
-                        Image.network(   //s'utilitza per a carregar imatges des de internet
-                          news.image!, // si no hi ha imatge, es mostra la sized box d'abaix
-                          fit: BoxFit.cover,   // ajusta la imatge a la mida del sizedBox
-                        ),
-                      SizedBox(height: 8),  // aquesta sized box es mostra sempre 
-                    ],
-                  ),
-                  SizedBox(height: 8),
-                  Card(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0), // equilibrem distancies en tots els costats de la tarjeta
-                      child: Column(
-                        children: [
-                          Text(
-                            'Url de la noticia:',
-                            style: TextStyle(fontSize: 16),
-                            softWrap: true, // trenca la linia si es necesari
+                    SizedBox(height: 8),
+                    Text(
+                      news.description ?? 'No description for this new', 
+                      style: TextStyle(fontSize: 16),
+                      softWrap: true, // trenca la linia si es necesari
+                    ),
+                    SizedBox(height: 8),
+                    Column(
+                      children: [
+                        if (news.image != null)
+                          Image.network(   //s'utilitza per a carregar imatges des de internet
+                            news.image!, // si no hi ha imatge, es mostra la sized box d'abaix
+                            fit: BoxFit.cover,   // ajusta la imatge a la mida del sizedBox
                           ),
-                          SizedBox(height: 8),
-                          Text(
-                            news.url ?? 'No url for this new', 
-                            style: TextStyle(fontSize: 16),
-                            softWrap: true,// trenca la linia siés necesari
-                          ),
-                          ElevatedButton(
+                        SizedBox(height: 8),  // aquesta sized box es mostra sempre 
+                      ],
+                    ),
+                    SizedBox(height: 8),
+                    Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0), // equilibrem distancies en tots els costats de la tarjeta
+                        child: Column(
+                          children: [
+                            if (news.url != null && news.url!.isNotEmpty)...[ // condicionem que hi hagi URL per a mostrar el text + el botó de la URL
+                              Text(
+                                'Url de la noticia:',
+                                style: TextStyle(fontSize: 16),
+                                softWrap: true, // trenca la linia si es necesari
+                              ),
+                              SizedBox(height: 8),
                             
-                            onPressed: (){
-                              _launchURL(news.url);
-                              },// _launchURL(news.url),
- 
-                            child: Text("Obrir noticia al navegador"),
-                          ),
-                        ],
+                              Text(
+                                news.url ?? 'No url for this new', 
+                                style: TextStyle(fontSize: 16),
+                                softWrap: true,// trenca la linia siés necesari
+                              ),
+                              ElevatedButton(
+                                onPressed: (){
+                                  _launchURL(news.url);
+                                  },// _launchURL(news.url),
+          
+                                child: Text("Obrir noticia al navegador"),
+                              ),
+                            ] else...[
+                              Text("Contacta amb el supervisor per a obtenir la URL de la noticia")
+                            ], 
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -212,12 +216,7 @@ Future<void> _launchURL(String? url) async {
     throw Exception('URL is null or empty');
   }
   final Uri uri = Uri.parse(url);
-
- // if (Platform.isWindows) {
-   // Process.run('explorer.exe', [uri.toString()]); // Abre el navegador en Windows
-   //} else {
-    if (!await launchUrl(uri)){
-      throw Exception('Could not launch $uri');
-    }
-  //}
+  if (!await launchUrl(uri)){
+    throw Exception('Could not launch $uri');
+  }
 }
